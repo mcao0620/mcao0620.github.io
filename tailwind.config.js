723155f28dfd36f7876be626cad16af32c47a07c
Michael Cao <michaelcao4@gmail.com>
2026-01-07 23:55:38 -0800

    update theming

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,3 @@
-// tailwind.config.js
-
-let colors = require("tailwindcss/colors");
-
 module.exports = {
   content: [
     "./*.html",
@@ -9,7 +5,32 @@ module.exports = {
   ],
   darkMode: "class",
   theme: {
-    colors: colors,
+    extend: {
+      colors: {
+        // Bamboo.nvim-inspired palette (vulgaris variant)
+        bambooBg: "#252623",        // vulgaris.bg0
+        bambooBgDark: "#1c1e1b",    // vulgaris.bg_d
+        bambooBg1: "#2f312c",       // vulgaris.bg1
+        bambooBg2: "#383b35",       // vulgaris.bg2
+        bambooBg3: "#3a3d37",       // vulgaris.bg3
+        bambooFg: "#f1e9d2",        // vulgaris.fg
+        bambooContrast: "#111210",  // vulgaris.contrast
+        bambooInverse: "#ffffff",   // vulgaris.inverse
+        bambooMuted: "#838781",     // vulgaris.light_grey
+        bambooGrey: "#5b5e5a",      // vulgaris.grey
+        bambooCard: "#ece1c0",      // softer light neutral (multiplex.fg)
+        bambooGreen: "#8fb573",     // vulgaris.green
+        bambooBlue: "#57a5e5",      // vulgaris.blue
+        bambooLightBlue: "#96c7ef", // vulgaris.light_blue
+        bambooOrange: "#ff9966",    // vulgaris.orange
+        bambooYellow: "#dbb651",    // vulgaris.yellow
+        bambooCyan: "#70c2be",      // vulgaris.cyan
+        bambooRed: "#e75a7c",       // vulgaris.red
+        bambooCoral: "#f08080",     // vulgaris.coral
+        bambooPurple: "#aaaaff",    // vulgaris.purple
+        bambooBrightPurple: "#df73ff", // vulgaris.bright_purple
+      },
+    },
   },
   variants: {},
   plugins: [],
